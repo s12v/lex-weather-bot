d5e80ae764adebe714731c46af8f2a1f80cb9f95
Sergey Novikov <snov@snov.me>
2017-05-22 13:20:25 +0200

    wip1

--- lambda.py
+++ lambda.py
@@ -1,13 +1,3 @@
-"""
-This sample demonstrates an implementation of the Lex Code Hook Interface
-in order to serve a sample bot which manages reservations for hotel rooms and car rentals.
-Bot, Intent, and Slot models which are compatible with this sample can be found in the Lex Console
-as part of the 'BookTrip' template.
-
-For instructions on how to set up and test this bot, as well as additional samples,
-visit the Lex Getting Started documentation http://docs.aws.amazon.com/lex/latest/dg/getting-started.html.
-"""
-
 import json
 import datetime
 import time
@@ -73,15 +63,6 @@ def delegate(session_attributes, slots):
 # --- Helper Functions ---
 
 
-def safe_int(n):
-    """
-    Safely convert n value to int.
-    """
-    if n is not None:
-        return int(n)
-    return n
-
-
 def try_ex(func):
     """
     Call passed in function in try block. If KeyError is encountered return None.
@@ -96,58 +77,7 @@ def try_ex(func):
         return None
 
 
-def generate_car_price(location, days, age, car_type):
-    """
-    Generates a number within a reasonable range that might be expected for a flight.
-    The price is fixed for a given pair of locations.
-    """
-
-    car_types = ['economy', 'standard', 'midsize', 'full size', 'minivan', 'luxury']
-    base_location_cost = 0
-    for i in range(len(location)):
-        base_location_cost += ord(location.lower()[i]) - 97
-
-    age_multiplier = 1.10 if age < 25 else 1
-    # Select economy is car_type is not found
-    if car_type not in car_types:
-        car_type = car_types[0]
-
-    return days * ((100 + base_location_cost) + ((car_types.index(car_type.lower()) * 50) * age_multiplier))
-
-
-def generate_hotel_price(location, nights, room_type):
-    """
-    Generates a number within a reasonable range that might be expected for a hotel.
-    The price is fixed for a pair of location and roomType.
-    """
-
-    room_types = ['queen', 'king', 'deluxe']
-    cost_of_living = 0
-    for i in range(len(location)):
-        cost_of_living += ord(location.lower()[i]) - 97
-
-    return nights * (100 + cost_of_living + (100 + room_types.index(room_type.lower())))
-
-
-def isvalid_car_type(car_type):
-    car_types = ['economy', 'standard', 'midsize', 'full size', 'minivan', 'luxury']
-    return car_type.lower() in car_types
-
-
-def isvalid_city(city):
-    valid_cities = ['new york', 'los angeles', 'chicago', 'houston', 'philadelphia', 'phoenix', 'san antonio',
-                    'san diego', 'dallas', 'san jose', 'austin', 'jacksonville', 'san francisco', 'indianapolis',
-                    'columbus', 'fort worth', 'charlotte', 'detroit', 'el paso', 'seattle', 'denver', 'washington dc',
-                    'memphis', 'boston', 'nashville', 'baltimore', 'portland']
-    return city.lower() in valid_cities
-
-
-def isvalid_room_type(room_type):
-    room_types = ['queen', 'king', 'deluxe']
-    return room_type.lower() in room_types
-
-
-def isvalid_date(date):
+def is_valid_date(date):
     try:
         dateutil.parser.parse(date)
         return True
@@ -155,57 +85,30 @@ def isvalid_date(date):
         return False
 
 
-def get_day_difference(later_date, earlier_date):
-    later_datetime = dateutil.parser.parse(later_date).date()
-    earlier_datetime = dateutil.parser.parse(earlier_date).date()
-    return abs(later_datetime - earlier_datetime).days
+class ValidationError(Exception):
+    def __init__(self, slot, message):
+        super(ValidationError, self).__init__(message)
+        self.slot = slot
+        self.message = message
 
 
-def add_days(date, number_of_days):
-    new_date = dateutil.parser.parse(date).date()
-    new_date += datetime.timedelta(days=number_of_days)
-    return new_date.strftime('%Y-%m-%d')
+def validate_request(slots):
+    city = slots.get('City')
+    country = slots.get('Country')
+    date = slots.get('Date')
 
+    # No city
+    if not city:
+        raise ValidationError('City', 'Which city?')
 
-def build_validation_result(isvalid, violated_slot, message_content):
-    return {
-        'isValid': isvalid,
-        'violatedSlot': violated_slot,
-        'message': {'contentType': 'PlainText', 'content': message_content}
-    }
+    # No date
+    if not date:
+        raise ValidationError('Date', 'Please provide a date')
 
-
-def validate_hotel(slots):
-    location = try_ex(lambda: slots['Location'])
-    checkin_date = try_ex(lambda: slots['CheckInDate'])
-    nights = safe_int(try_ex(lambda: slots['Nights']))
-    room_type = try_ex(lambda: slots['RoomType'])
-
-    if location and not isvalid_city(location):
-        return build_validation_result(
-            False,
-            'Location',
-            'We currently do not support {} as a valid destination.  Can you try a different city?'.format(location)
-        )
-
-    if checkin_date:
-        if not isvalid_date(checkin_date):
-            return build_validation_result(False, 'CheckInDate',
-                                           'I did not understand your check in date.  When would you like to check in?')
-        if datetime.datetime.strptime(checkin_date, '%Y-%m-%d').date() <= datetime.date.today():
-            return build_validation_result(False, 'CheckInDate',
-                                           'Reservations must be scheduled at least one day in advance.  Can you try a different date?')
-
-    if nights is not None and (nights < 1 or nights > 30):
-        return build_validation_result(
-            False,
-            'Nights',
-            'You can make a reservations for from one to thirty nights.  How many nights would you like to stay for?'
-        )
-
-    if room_type and not isvalid_room_type(room_type):
-        return build_validation_result(False, 'RoomType',
-                                       'I did not recognize that room type.  Would you like to stay in a queen, king, or deluxe room?')
+    # Invalid date
+    if date:
+        if not is_valid_date(date):
+            raise ValidationError('Date', 'I did not understand date. Could you please enter it again?')
 
     return {'isValid': True}
 
@@ -213,110 +116,54 @@ def validate_hotel(slots):
 """ --- Functions that control the bot's behavior --- """
 
 
-def book_hotel(intent_request):
-    """
-    Performs dialog management and fulfillment for booking a hotel.
+def weather(intent_request):
 
-    Beyond fulfillment, the implementation for this intent demonstrates the following:
-    1) Use of elicitSlot in slot validation and re-prompting
-    2) Use of sessionAttributes to pass information that can be used to guide conversation
-    """
+    # load city from google (i.e. validate)
+    # load data from api
+    # respond with data
 
-    location = try_ex(lambda: intent_request['currentIntent']['slots']['Location'])
-    checkin_date = try_ex(lambda: intent_request['currentIntent']['slots']['CheckInDate'])
-    nights = safe_int(try_ex(lambda: intent_request['currentIntent']['slots']['Nights']))
+    city = try_ex(lambda: intent_request['currentIntent']['slots']['City'])
+    country = try_ex(lambda: intent_request['currentIntent']['slots']['Country'])
+    date = try_ex(lambda: intent_request['currentIntent']['slots']['Date'])
 
-    room_type = try_ex(lambda: intent_request['currentIntent']['slots']['RoomType'])
     session_attributes = intent_request['sessionAttributes'] if intent_request['sessionAttributes'] is not None else {}
-
-    # Load confirmation history and track the current reservation.
-    reservation = json.dumps({
-        'ReservationType': 'Hotel',
-        'Location': location,
-        'RoomType': room_type,
-        'CheckInDate': checkin_date,
-        'Nights': nights
+    user_data = json.dumps({
+        'city': city,
+        'country': country,
+        'date': date
     })
-
-    session_attributes['currentReservation'] = reservation
+    session_attributes['user_data'] = user_data
 
     if intent_request['invocationSource'] == 'DialogCodeHook':
-        # Validate any slots which have been specified.  If any are invalid, re-elicit for their value
-        validation_result = validate_hotel(intent_request['currentIntent']['slots'])
-        if not validation_result['isValid']:
+        try:
+            validate_request(intent_request['currentIntent']['slots'])
+        except ValidationError as err:
             slots = intent_request['currentIntent']['slots']
-            slots[validation_result['violatedSlot']] = None
-
+            slots[err.slot] = None
             return elicit_slot(
                 session_attributes,
                 intent_request['currentIntent']['name'],
                 slots,
-                validation_result['violatedSlot'],
-                validation_result['message']
+                err.slot,
+                {'contentType': 'PlainText', 'content': err.message}
             )
 
-        # Otherwise, let native DM rules determine how to elicit for slots and prompt for confirmation.  Pass price
-        # back in sessionAttributes once it can be calculated; otherwise clear any setting from sessionAttributes.
-        if location and checkin_date and nights and room_type:
-            # The price of the hotel has yet to be confirmed.
-            price = generate_hotel_price(location, nights, room_type)
-            session_attributes['currentReservationPrice'] = price
-        else:
-            try_ex(lambda: session_attributes.pop('currentReservationPrice'))
-
-        session_attributes['currentReservation'] = reservation
         return delegate(session_attributes, intent_request['currentIntent']['slots'])
 
-    # Booking the hotel.  In a real application, this would likely involve a call to a backend service.
-    logger.debug('bookHotel under={}'.format(reservation))
-
-    try_ex(lambda: session_attributes.pop('currentReservationPrice'))
-    try_ex(lambda: session_attributes.pop('currentReservation'))
-    session_attributes['lastConfirmedReservation'] = reservation
+    logger.debug('do stuff')
 
     return close(
         session_attributes,
         'Fulfilled',
         {
             'contentType': 'PlainText',
-            'content': 'Thanks, I have placed your reservation.   Please let me know if you would like to book a car '
-                       'rental, or another hotel.'
+            'content': 'Done!!'
         }
     )
-
-
-# --- Intents ---
-
-
-def dispatch(intent_request):
-    """
-    Called when the user specifies an intent for this bot.
-    """
-
-    logger.debug(
-        'dispatch userId={}, intentName={}'.format(intent_request['userId'], intent_request['currentIntent']['name'])
-    )
-
-    intent_name = intent_request['currentIntent']['name']
-
-    # Dispatch to your bot's intent handlers
-    if intent_name == 'BookHotel':
-        return book_hotel(intent_request)
-
-    raise Exception('Intent with name ' + intent_name + ' not supported')
-
 
 # --- Main handler ---
 
 
 def lambda_handler(event, context):
-    """
-    Route the incoming request based on intent.
-    The JSON body of the request is provided in the event slot.
-    """
-    # By default, treat the user request as coming from the America/New_York time zone.
-    os.environ['TZ'] = 'America/New_York'
-    time.tzset()
-    logger.debug('event.bot.name={}'.format(event['bot']['name']))
-
-    return dispatch(event)
+    logger.debug('event={}'.format(json.dumps(event)))
+    return weather(event)
